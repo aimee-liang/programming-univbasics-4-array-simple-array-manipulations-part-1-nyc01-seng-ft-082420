8c242ca0f1941263e83ffb8a3aa0c57c6f43a263
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 18:29:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,25 @@
-def using_push
+def using_push(array, message)
+  array.push(message)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+  return array
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+  return array
 end
